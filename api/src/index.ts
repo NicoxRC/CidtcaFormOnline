d182a4343a8bdcbbe218de9b0348d3a001924dcb
NicoxRC <nicorojas005@gmail.com>
2022-12-12 17:38:08 -0500

    production

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,8 +4,8 @@ import { sequelize } from "./database/database";
 async function main() {
   try {
     await sequelize.sync({ force: false });
-    app.listen(3001, () => {
-      console.log("listening on port", 3001);
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port", process.env.PORT);
     });
   } catch (error) {
     console.log("error", error);
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import dotenv from "dotenv";
+import axios from "axios";
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
